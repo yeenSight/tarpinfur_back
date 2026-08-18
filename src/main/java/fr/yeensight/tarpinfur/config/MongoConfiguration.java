1e93cce481ef95e9dada77572238e14938d532c8
Kelkun <kelkunfur@gmail.com>
2026-08-18 11:05:58 +0200

    bug - use mongo conf for test and get value

--- src/main/java/fr/yeensight/tarpinfur/config/MongoConfiguration.java
+++ src/main/java/fr/yeensight/tarpinfur/config/MongoConfiguration.java
@@ -12,16 +12,20 @@ import com.mongodb.ConnectionString;
 
 @Configuration
 @EnableMongoRepositories(basePackages = "fr.yeensight.tarpinfur.repository")
-@Profile("!test")
+@Profile("!test") // n'activer cette configuration que hors profil "test"
 public class MongoConfiguration extends AbstractMongoClientConfiguration {
 
-    @Value("${spring.data.mongodb.host}")
+    // On preferera une URI complete si fournie
+    @Value("${spring.data.mongodb.uri:}")
+    private String uri;
+
+    @Value("${spring.data.mongodb.host:localhost}")
     private String host;
 
-    @Value("${spring.data.mongodb.port}")
+    @Value("${spring.data.mongodb.port:27017}")
     private String port;
 
-    @Value("${spring.data.mongodb.database}")
+    @Value("${spring.data.mongodb.database:tarpinfur_db}")
     private String database;
 
     @Value("${spring.data.mongodb.username:}")
@@ -40,11 +44,15 @@ public class MongoConfiguration extends AbstractMongoClientConfiguration {
 
     @Override
     public MongoClient mongoClient() {
+        if (uri != null && !uri.isBlank()) {
+            return MongoClients.create(new ConnectionString(uri));
+        }
+
         String connectionString;
         if (username != null && !username.isBlank()) {
             connectionString = String.format(
                     "mongodb://%s:%s@%s:%s/%s?authSource=%s",
-                    username, password, host, port, database, authDatabase
+                    username, password, host, port, database, authDatabase == null || authDatabase.isBlank() ? database : authDatabase
             );
         } else {
             connectionString = String.format(
